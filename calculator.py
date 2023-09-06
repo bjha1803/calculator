a8863e1a09148f351e2c1bccff27a1d77a5fe6fd
bjha1803 <144241573+bjha1803@users.noreply.github.com>
2023-09-06 18:35:43 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+n1=  int(input ("Enter your 1st number :"))
+n2=  int(input ("Enter your 2nd number :"))
+n3=  input ("Enter the operator (+,-,*,/) :")
+if n3=="+":
+    print(n1 + n2)
+elif n3=="-":
+        print(n1-n2)
+elif n3=="*":
+            print(n1*n2)
+elif n3=="/":
+                print(n1/n2)
+else:
+ print("invalid operator")
+
+
+    
